02cbcee3e62d944afdaf465aa941992478976d90
Abdullah Beyaztaş <beyaz1404@gmail.com>
2019-12-30 08:39:03 +0300

    .

---
+++ BOA.EntityGeneration.ConstantsProjectGeneration.Test/GeneratorTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BOA.EntityGeneration.ConstantsProjectGeneration
+{
+    [TestClass]
+    public class GeneratorTest
+    {
+        [TestMethod]
+        public void When_profile_name_starts_with_CRD_it__should_starts_with_CreditCard()
+        {
+            Smocks.Smock.Run((context) =>
+            {
+
+                var generator = new Generator();
+                generator.Generate();
+            });
+            
+        }
+    }
+}
