b2ae4803d840dfdc7c92c93a74568b70b27ce877
Abdullah Beyaztaş <beyaz1404@gmail.com>
2019-12-30 11:46:52 +0300

    .

--- BOA.EntityGeneration.CustomSQLExporting.Test/CustomSqlExporterTest.cs
+++ BOA.EntityGeneration.CustomSQLExporting.Test/CustomSqlExporterTest.cs
@@ -1,5 +1,6 @@
-﻿using System;
+﻿using BOA.EntityGeneration.CustomSQLExporting.Wrapper;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Smocks;
 
 namespace BOA.EntityGeneration.CustomSQLExporting
 {
@@ -9,8 +10,12 @@ namespace BOA.EntityGeneration.CustomSQLExporting
         [TestMethod]
         public void When_input_parameter_ends_with_FLAG_and_value_type_is_char_one_it_should_generate_dot_net_boolean_property()
         {
+            Smock.Run(context =>
+            {
+                var exporter = new CustomSqlExporter();
 
+                exporter.Export(string.Empty);
+            });
         }
-
     }
 }
