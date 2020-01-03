d9fe4ea87c913604ceac97f555037de162de73c4
Abdullah Beyaztaş <beyaz1404@gmail.com>
2020-01-03 09:52:59 +0300

    .

--- BOA.EntityGeneration/ScriptModel/Creators/ContractBodyDbMembersCreator.cs
+++ BOA.EntityGeneration/ScriptModel/Creators/ContractBodyDbMembersCreator.cs
@@ -8,6 +8,47 @@ using DotNetStringUtilities;
 
 namespace BOA.EntityGeneration.ScriptModel.Creators
 {
+    class ContractDbMemberWriter
+    {
+        #region Public Properties
+        public IReadOnlyList<string> Comments     { get; set; }
+        public string                DotNetType   { get; set; }
+        public string                PropertyName { get; set; }
+        #endregion
+
+        #region Properties
+        static string PaddingForComment => "     ";
+        #endregion
+
+        #region Public Methods
+        public void Write(StringBuilder sb)
+        {
+            if (Comments.Any())
+            {
+                sb.AppendLine("/// <summary>");
+
+                var isFirstComment = true;
+                foreach (var item in Comments)
+                {
+                    if (isFirstComment)
+                    {
+                        isFirstComment = false;
+                        sb.AppendLine("///" + PaddingForComment + "" + item);
+                    }
+                    else
+                    {
+                        sb.AppendLine("///" + PaddingForComment + "<para> " + item + " </para>");
+                    }
+                }
+
+                sb.AppendLine(@"/// </summary>");
+            }
+
+            sb.AppendLine("public " + DotNetType + " " + PropertyName + " { get; set; }");
+        }
+        #endregion
+    }
+
     /// <summary>
     ///     The contract body database members creator
     /// </summary>
@@ -31,14 +72,7 @@ namespace BOA.EntityGeneration.ScriptModel.Creators
             sb.AppendLine("#region Database Columns");
             foreach (var columnInfo in TableInfo.Columns)
             {
-                var extraComment = string.Empty;
-
-                var indexInfo = TableInfo.IndexInfoList.FirstOrDefault(x => x.ColumnNames.Contains(columnInfo.ColumnName));
-
-                if (indexInfo != null)
-                {
-                    extraComment = indexInfo.ToString();
-                }
+                var extraComment = GetIndexComment(TableInfo, columnInfo);
 
                 sb.AppendLine();
                 Write(sb, columnInfo, extraComment);
@@ -52,6 +86,56 @@ namespace BOA.EntityGeneration.ScriptModel.Creators
         #endregion
 
         #region Methods
+        static ContractDbMemberWriter CreateContractDbMemberWriter(ITableInfo tableInfo, IColumnInfo data)
+        {
+            var dotNetType = data.DotNetType;
+            if (data.ColumnName == Names2.VALID_FLAG)
+            {
+                dotNetType = DotNetTypeName.DotNetBool;
+            }
+
+            var comment = data.Comment;
+
+            var commentList = new List<string>();
+
+            if (comment.HasValue())
+            {
+                commentList.AddRange(comment.Split(Environment.NewLine.ToCharArray()));
+            }
+
+            var extraComment = GetIndexComment(tableInfo, data);
+            if (extraComment.HasValue())
+            {
+                commentList.Add(extraComment);
+            }
+
+            return new ContractDbMemberWriter
+            {
+                PropertyName = data.ColumnName.ToContractName(),
+                DotNetType   = dotNetType,
+                Comments     = commentList
+            };
+        }
+
+        static string GetIndexComment(ITableInfo tableInfo, IColumnInfo columnInfo)
+        {
+            var extraComment = string.Empty;
+
+            var indexInfo = tableInfo.IndexInfoList.FirstOrDefault(x => x.ColumnNames.Contains(columnInfo.ColumnName));
+
+            if (indexInfo != null)
+            {
+                extraComment = indexInfo.ToString();
+            }
+
+            return extraComment;
+        }
+
+        static IReadOnlyList<ContractDbMemberWriter> GetMembers(ITableInfo tableInfo)
+        {
+            return tableInfo.Columns.Select(info => CreateContractDbMemberWriter(tableInfo, info)).ToList();
+        }
+
         /// <summary>
         ///     Writes the specified sb.
         /// </summary>
